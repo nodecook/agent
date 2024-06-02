29789be48ea1cf0809d14b29d41ebfab987a0938
long2ice <long2ice@gmail.com>
2024-06-02 14:42:26 +0800

    fix: ping

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main() {
         error!("ipv4_only and ipv6_only can't be true at the same time");
         exit(1);
     }
-    let (tx, mut rx) = mpsc::channel::<String>(2);
+    let (tx, mut rx) = mpsc::channel::<String>(10);
     let mut v4_ok = false;
     let mut v6_ok = false;
     let v4_server = args
